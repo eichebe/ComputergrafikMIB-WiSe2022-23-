b013c6e2e41f322dc6d5153d5a35d8a64f3cc0ca
eichebe <bennyeiche1@web.de>
2022-12-13 19:31:17 +0100

    Abgabe erste Version

--- Tut08_FirstSteps/Tut08_FirstSteps.cs
+++ Tut08_FirstSteps/Tut08_FirstSteps.cs
@@ -29,6 +29,14 @@ namespace FuseeApp
 
         private SurfaceEffect _cubeEffect;
 
+        private CuboidMesh _cubeMesh;
+
+        private Transform _cubeTransform2;
+
+        private Transform _longCubeTransform;
+
+        private Transform _randomCubeTransform;
+
 
         // Init is called on startup. 
         public override void Init()
@@ -42,33 +50,69 @@ namespace FuseeApp
             // A node containing one Camera component.
             _camera = new Camera(ProjectionMethod.Perspective, 5, 100, M.PiOver4)
             {
-                BackgroundColor = new float4(0.5f, 0.5f, 1.0f, 1.0f) //(float4)ColorUint.Greenery
+                BackgroundColor = (float4)ColorUint.DarkGray
             };
 
             var cameraNode = new SceneNode();
             cameraNode.Components.Add(_camera);
             _cameraTransform = new Transform{ Translation = new float3(0, 0, -50)};
-
+            cameraNode.Components.Add(_cameraTransform);
+            cameraNode.Components.Add(_camera);
             // Create the scene containing the cube as the only object
             _scene = new SceneContainer();
             _scene.Children.Add(cameraNode);
 
             //THE CUBE
-            
-            _cubeTransform= new Transform{ Translation = new float3(0, 0, 50), Rotation = new float3(0,45,0)};
-            
+            //Original Cube
+            _cubeTransform = new Transform{Rotation = new float3(0,45,0)};
             _cubeEffect = MakeEffect.FromDiffuseSpecular((float4)ColorUint.Blue);
-            var cubeMesh = new CuboidMesh(new float3(10, 10, 10));
-
+            _cubeMesh = new CuboidMesh(new float3(10, 10, 10));
             var cubeNode = new SceneNode();
             cubeNode.Components.Add(_cubeTransform);
             cubeNode.Components.Add(_cubeEffect);
-            cubeNode.Components.Add(cubeMesh);
+            cubeNode.Components.Add(_cubeMesh);
+
+            //Cube Rotated to create a star           
+            var cubeMesh2 = new CuboidMesh(new float3(10, 10, 10));
+            var cubeNode2 = new SceneNode();
+            _cubeTransform2 = new Transform{Translation = new float3(100,100,0)};
+            _cubeTransform2 = new Transform{Rotation = new float3(0,10,0)};
+            var cubeEffect2 = MakeEffect.FromDiffuseSpecular((float4)ColorUint.Red);
+            
+            
+            cubeNode.Components.Add(_cubeTransform2);
+            cubeNode.Components.Add(cubeEffect2);
+            cubeNode.Components.Add(cubeMesh2);
+
+            //Up down long Cube
+            _longCubeTransform = new Transform{Translation = new float3(30,1,20)}; 
+            CuboidMesh longCube = new CuboidMesh(new float3(10,40,10));
+            var longCubeEffect = MakeEffect.FromDiffuseSpecular((float4)ColorUint.Gold);
+            var longCubeNode = new SceneNode();
+            longCubeNode.Components.Add(_longCubeTransform);
+            longCubeNode.Components.Add(longCubeEffect);
+            longCubeNode.Components.Add(longCube);
+
+            //Random Snowflake Cubes
+            
+            for(int i = 0; i<60; i++){
+            Random rd = new Random();
+            var randomCubeEffect = MakeEffect.FromDiffuseSpecular((float4)ColorUint.Snow);
+            _randomCubeTransform = new Transform{Translation = new float3(rd.Next(0,60),rd.Next(0,60),0)};
+            CuboidMesh randomCube = new CuboidMesh(new float3(1,1,1));
+            longCubeNode.Components.Add(_randomCubeTransform);
+            longCubeNode.Components.Add(randomCubeEffect);
+            longCubeNode.Components.Add(randomCube);
+            }
+            
 
             //THE SCENE
             _scene = new SceneContainer();
             _scene.Children.Add(cameraNode);
             _scene.Children.Add(cubeNode);
+            _scene.Children.Add(longCubeNode);
+           
+            
             
             
             //THE SCENE RENDERER
@@ -79,9 +123,16 @@ namespace FuseeApp
         // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
-            _cubeTransform.Rotation = new float3(0, _cubeTransform.Rotation.y +0.01f, 0);
+            
+            _cubeTransform.Rotation = new float3( _cubeTransform.Rotation.y +0.05f,_cubeTransform.Rotation.y +0.05f,0);
 
-            _cubeTransform.Translation = new float3(5 * M.Sin(3 * TimeSinceStart),0,0);
+            _cubeTransform.Translation = new float3(5 * M.Sin(3 * TimeSinceStart),5 * M.Cos(3 * TimeSinceStart),0);
+            
+            _longCubeTransform.Translation = new float3(30,20*M.Cos(6 * TimeSinceStart), 20);
+            Random rd = new Random();
+            for(int i = 0; i<60; i++){
+            _randomCubeTransform = new Transform{Translation = new float3(rd.Next(0,60),rd.Next(0,60),0)};
+            }
 
             //change color per second
             _cubeEffect.SurfaceInput.Albedo = new float4(M.Sin(3 * Time.TimeSinceStart) *0.5f + 0.5f, //Rotkanal 
