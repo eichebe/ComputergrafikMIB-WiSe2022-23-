53fa92ede0e04f36251c68125e0c88d7db98bda1
Benedikt <bennyeiche1@web.de>
2023-01-05 12:36:30 +0100

    Aufgabe 9 fertig

--- Tut09_HierarchyAndInput/Tut09_HierarchyAndInput.cs
+++ Tut09_HierarchyAndInput/Tut09_HierarchyAndInput.cs
@@ -21,17 +21,66 @@ namespace FuseeApp
     {
         private SceneContainer _scene;
         private SceneRendererForward _sceneRenderer;
+        private Transform _camAngle;
         private Transform _baseTransform;
+        private Transform _bodyTransform;
+        private Transform _upperRobotArmTransform;
+        private Transform _frontRobotArmTransform;
+        private Transform _robotHandArmLeftTransform;
+        private Transform _robotHandArmRightTransform;
+        private float _cameraSpeed;
+        private Transform _robotUpperArmTransform;
+        private Boolean _cameraMoved;
+        private Boolean _robotHandOpen = false;
+        private Boolean _robotHandClosed = false;
 
 
         SceneContainer CreateScene()
         {
             // Initialize transform components that need to be changed inside "RenderAFrame"
+            _camAngle = new Transform
+            {
+                Translation = new float3(0, 0, 0),
+            };
+
             _baseTransform = new Transform
             {
                 Rotation = new float3(0, 0, 0),
                 Scale = new float3(1, 1, 1),
                 Translation = new float3(0, 0, 0)
+            };
+
+            _bodyTransform = new Transform
+            {
+                Translation = new float3(0, 6, 0),
+                Rotation = new float3(0, (float) Math.PI/2, 0)
+            };
+
+            _upperRobotArmTransform = new Transform
+            {
+                Translation = new float3(2, 4, 0),
+                Rotation = new float3((float) Math.PI/4, 0, 0)
+            };
+
+            _frontRobotArmTransform = new Transform
+            {
+                Translation = new float3(-2, 4, 0),
+                Rotation = new float3((float) Math.PI/3, 0, 0)
+            };
+
+            _robotHandArmLeftTransform = new Transform
+            {
+                Translation = new float3(0, 4, 0),
+            };
+
+            _robotHandArmRightTransform = new Transform
+            {
+                Translation = new float3(0, 4, 0),
+            };
+
+            _robotUpperArmTransform = new Transform
+            {
+                Translation = new float3(0, 4, 0),
             };
 
             // Setup the scene graph
@@ -44,20 +93,30 @@ namespace FuseeApp
                         Name = "Camera",
                         Components =
                         {
-                            new Transform
+                            _camAngle
+                        },
+                        Children =
+                        {
+                            new SceneNode
                             {
-                                Translation = new float3(0, 10, -50),
-                            },
-                            new Camera(ProjectionMethod.Perspective, 5, 100, M.PiOver4)
-                            {
-                                BackgroundColor =  (float4) ColorUint.Greenery
+                                Components =
+                                {
+                                    new Transform
+                                    {
+                                        Translation = new float3(0, 10, -50),
+                                    },
+                                    new Camera(ProjectionMethod.Perspective, 5, 100, M.PiOver4)
+                                    {
+                                        BackgroundColor =  (float4) ColorUint.Greenery
+                                    }
+                                }
                             }
                         }
                     },
 
                     new SceneNode
                     {
-                        Name = "Robot",
+                        Name = "Base",
                         Components =
                         {
                             // TRANSFORM COMPONENT
@@ -68,6 +127,139 @@ namespace FuseeApp
 
                             // MESH COMPONENT
                             new CuboidMesh(new float3(10, 2, 10))
+                        },
+                        Children =
+                        {
+                            new SceneNode
+                            {
+                                Name = "Robot",
+                                Components = 
+                                {
+                                    _bodyTransform,
+                                    MakeEffect.FromDiffuseSpecular((float4) ColorUint.IndianRed),
+                                    new CuboidMesh(new float3(2, 10, 2))
+                                },
+                                Children =
+                                {
+                                    new SceneNode
+                                    {
+                                        Name = "UpperArm",
+                                        Components = 
+                                        {
+                                            _upperRobotArmTransform,
+                                        },
+                                        Children = 
+                                        {
+                                            new SceneNode
+                                            {
+                                                Components =
+                                                {
+                                                    new Transform { Translation = new float3(0, 4, 0)},
+                                                    MakeEffect.FromDiffuseSpecular((float4) ColorUint.ForestGreen),
+                                                    new CuboidMesh(new float3(2, 10, 2))
+                                                },
+                                                Children =
+                                                {
+                                                    new SceneNode
+                                                    {
+                                                        Name = "FrontArm",
+                                                        Components = 
+                                                        {
+                                                            _frontRobotArmTransform,
+                                                        },
+                                                        Children = 
+                                                        {
+                                                            new SceneNode
+                                                            {
+                                                                Components =
+                                                                {
+                                                                    new Transform { Translation = new float3(0, 4, 0)},
+                                                                    MakeEffect.FromDiffuseSpecular((float4) ColorUint.Blue),
+                                                                    new CuboidMesh(new float3(2, 10, 2))
+                                                                },
+                                                                Children =
+                                                                {
+                                                                    new SceneNode
+                                                                    {
+                                                                        Name = "RobotHandArmLeft",
+                                                                        Components = 
+                                                                        {
+                                                                            _robotHandArmLeftTransform,
+                                                                        },
+                                                                        Children = 
+                                                                        {
+                                                                            new SceneNode
+                                                                            {
+                                                                                Components =
+                                                                                {
+                                                                                    new Transform 
+                                                                                    {
+                                                                                        Translation = new float3(1, 2, 1),
+                                                                                        Rotation = new float3(0, (float) Math.PI/4, 0)
+                                                                                    },
+                                                                                    MakeEffect.FromDiffuseSpecular((float4) ColorUint.LightGrey),
+                                                                                    new CuboidMesh(new float3(1, 3, 1))
+                                                                                }
+                                                                                
+                                                                            },
+                                                                        }
+                                                                    },
+                                                                    new SceneNode
+                                                                    {
+                                                                        Name = "RobotHandArmRight",
+                                                                        Components = 
+                                                                        {
+                                                                            _robotHandArmRightTransform,
+                                                                        },
+                                                                        Children =
+                                                                        {
+                                                                            new SceneNode
+                                                                            {
+                                                                                Components =
+                                                                                {
+                                                                                    new Transform 
+                                                                                    {
+                                                                                        Translation = new float3(-1, 2, 1),
+                                                                                        Rotation = new float3(0, (float) Math.PI/4, 0)
+                                                                                    },
+                                                                                    MakeEffect.FromDiffuseSpecular((float4) ColorUint.LightGrey),
+                                                                                    new CuboidMesh(new float3(1, 3, 1))
+                                                                                }
+                                                                                
+                                                                            }
+                                                                        }
+                                                                    },
+                                                                    new SceneNode
+                                                                    {
+                                                                        Name = "RobotHandUpperArm",
+                                                                        Components = 
+                                                                        {
+                                                                            _robotUpperArmTransform,
+                                                                        },
+                                                                        Children =
+                                                                        {
+                                                                            new SceneNode
+                                                                            {
+                                                                                Components =
+                                                                                {
+                                                                                    new Transform { Translation = new float3(0, 2, -1)},
+                                                                                    MakeEffect.FromDiffuseSpecular((float4) ColorUint.LightGrey),
+                                                                                    new CuboidMesh(new float3(1, 3, 1))
+                                                                                }
+                                                                                
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -78,20 +270,68 @@ namespace FuseeApp
         // Init is called on startup. 
         public override void Init()
         {
-            // Set the clear color for the backbuffer to white (100% intensity in all color channels R, G, B, A).
             RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
 
             _scene = CreateScene();
 
-            // Create a scene renderer holding the scene above
             _sceneRenderer = new SceneRendererForward(_scene);
         }
 
         // RenderAFrame is called once a frame
         public override void RenderAFrame()
         {
-            // Clear the backbuffer
             RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            if(Mouse.LeftButton) {
+                _cameraSpeed = Mouse.Velocity.x;
+                _camAngle.Rotation = new float3(0, _camAngle.Rotation.y + 0.5f*((float)Math.PI/180) * DeltaTime * _cameraSpeed, 0);
+                _cameraMoved = true;
+            }
+
+            if(_cameraMoved == true) {
+                _cameraSpeed = _cameraSpeed/1.1f;
+                _camAngle.Rotation = new float3(0, _camAngle.Rotation.y + 0.5f*((float)Math.PI/180) * DeltaTime * _cameraSpeed, 0);
+                if(_cameraSpeed > -0.1 & _cameraSpeed < 0.1) {
+                    _cameraMoved = false;
+                }
+            }
+
+            _bodyTransform.Rotation = new float3(0, _bodyTransform.Rotation.y + 90*((float)Math.PI/180) * DeltaTime * Keyboard.LeftRightAxis, 0);
+            _upperRobotArmTransform.Rotation = new float3(_upperRobotArmTransform.Rotation.x + 45*((float)Math.PI/180) * DeltaTime * Keyboard.UpDownAxis, 0, 0);
+            _frontRobotArmTransform.Rotation = new float3(_frontRobotArmTransform.Rotation.x + 60*((float)Math.PI/180) * DeltaTime * Keyboard.UpDownAxis, 0, 0);
+
+            if(Keyboard.GetKey(KeyCodes.D)) {
+                _robotHandOpen = true;
+                _robotHandClosed = false;
+            }
+                if(Keyboard.GetKey(KeyCodes.A)) {
+                _robotHandClosed = true;
+                _robotHandOpen = false;
+            }
+
+            
+
+            if(_robotHandClosed == true) {
+                float rotation = 10*((float)Math.PI/180);
+                if(_robotHandArmLeftTransform.Rotation.x <= rotation) {
+                    _robotHandArmLeftTransform.Rotation = new float3(_robotHandArmLeftTransform.Rotation.x + rotation * DeltaTime, 0, _robotHandArmLeftTransform.Rotation.z + (-rotation) * DeltaTime);
+                    _robotHandArmRightTransform.Rotation = new float3(_robotHandArmRightTransform.Rotation.x + rotation * DeltaTime, 0, _robotHandArmRightTransform.Rotation.z + rotation * DeltaTime);
+                    _robotUpperArmTransform.Rotation = new float3(_robotUpperArmTransform.Rotation.x + (-rotation) * DeltaTime, 0, 0);
+                } else {
+                    _robotHandClosed = false;
+                }
+            }
+
+            if(_robotHandOpen == true) {
+                float rotation = 10*((float)Math.PI/180);
+                if(_robotHandArmLeftTransform.Rotation.x >= -rotation) {
+                    _robotHandArmLeftTransform.Rotation = new float3(_robotHandArmLeftTransform.Rotation.x + (-rotation) * DeltaTime, 0, _robotHandArmLeftTransform.Rotation.z + rotation * DeltaTime);
+                    _robotHandArmRightTransform.Rotation = new float3(_robotHandArmRightTransform.Rotation.x + (-rotation) * DeltaTime, 0, _robotHandArmRightTransform.Rotation.z + (-rotation) * DeltaTime);
+                    _robotUpperArmTransform.Rotation = new float3(_robotUpperArmTransform.Rotation.x + rotation * DeltaTime, 0, 0);
+                } else {
+                    _robotHandOpen = false;
+                }
+            }
 
             // Render the scene on the current render context
             _sceneRenderer.Render(RC);
